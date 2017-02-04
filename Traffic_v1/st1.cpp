e7c22fcc03ae17a0cc12016628fdb5beafaddd98
Zero Weight <zwt15@mails.tsinghua.edu.cn>
2017-02-04 22:37:06 +0800

    Comment about the stategy

--- Traffic_v1/st1.cpp
+++ Traffic_v1/st1.cpp
@@ -1,4 +1,21 @@
 ﻿#include "traffic_v1.h"
-
+// the stategy of flowing
 void Traffic_v1::st1 () {
+	//check the head car
+		//check the shortest time arrive
+		//check the longest time arrive
+		//choose the shortest time possible to go to
+			//or choose the normal stategy
+		//cal. the time,acc etc
+	//check the car following
+		//check the shortest time and the longest time to ..
+		//choose the shortest time to arrive:block red and the safety driving
+			//or choose the normal stategy
+		//cal. the time ,acc etc
+
+	//for the basic stategy:
+		//same as the stategy given
+
+	//for cal. the saftydriving
+	//suppose the a
 }
